0afa4bf134100f836066c585539ed64ccebcd1ae
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2023-12-04 14:55:37 +0100

    button added to client side products and stock added to server side games.json

--- client/products/products.js
+++ client/products/products.js
@@ -1,14 +1,14 @@
+async function displayCurrentGames() {
+  const response = await fetch("/api/games");
+  const data = await response.json();
+  const games = data.games;
+  console.log(games);
 
-
-async function displayCurrentGames () {
-    const response = await fetch('/api/games')
-    const data = await response.json()
-    const games = data.games
-    console.log(games)
-
-    const root = document.querySelector('#root')
-    games.forEach(game => {
-        root.insertAdjacentHTML('beforeend', `
+  const root = document.querySelector("#root");
+  games.forEach((game) => {
+    root.insertAdjacentHTML(
+      "beforeend",
+      `
         <div class="container">
         <h2>Current Games:</h2>
         <p>
@@ -16,13 +16,16 @@ async function displayCurrentGames () {
         Name: ${game.name} 
         Year: ${game.year} 
         Price: ${game.price}$ 
-        Description: ${game.description}</p>
+        Description: ${game.description}
+        Stock: ${game.stock} pcs
+        </p>
+        <button class="action-button" data-game-id="${game.id}">Add product</button>        
         </div>
-        `)
-    });
+        `
+    );
+  });
 }
 
-
-document.addEventListener('DOMContentLoaded', async () => {
-    await displayCurrentGames();
-})
+document.addEventListener("DOMContentLoaded", async () => {
+  await displayCurrentGames();
+});
