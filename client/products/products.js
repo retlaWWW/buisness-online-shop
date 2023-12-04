fb4404d2579375949c6f4ab31c11ef7f7603212c
TiborKovari <142129834+TiborKovari@users.noreply.github.com>
2023-12-04 13:30:31 +0100

    client products dev

---
+++ client/products/products.js
@@ -0,0 +1,24 @@
+
+
+async function displayCurrentGames () {
+    const response = await fetch('/api/games')
+    const data = await response.json()
+    const games = data.games
+
+    const currentGamesDiv = document.getElementById('currentGames')
+    currentGamesDiv.innerHTML = '<h2>Current Games:</h2>'
+    if (games.length === 0) {
+        currentGamesDiv.innerHTML += '<p>No games available</p>'
+    } else {
+        games.forEach(game => {
+            currentGamesDiv.innerHTML += `
+            <p>ID: ${game.id} Name: ${game.name} Year: ${game.year} Price: ${game.price}$ Description: ${game.description}</p>
+            `                        
+        });
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    await displayCurrentGames();
+})
