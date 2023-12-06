0e7d4400f7766cbf24ed5ac155055f6241a03bd8
retlaWWW <frixonxd@gmail.com>
2023-12-06 17:02:38 +0100

    added functional + and - buttons and checkout button with no function yet

--- client/basket/basket.mjs
+++ client/basket/basket.mjs
@@ -1,3 +1,5 @@
+const prices = [];
+const quan = [];
 
 async function fetchGames() {
   try {
@@ -16,55 +18,65 @@ async function fetchBasket() {
   try {
     const response = await fetch("/api/basket");
     if (!response.ok) {
-      throw new Error("Network response was not ok")
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
     return data.basket;
   } catch (error) {
-    console.error("Error fetching basket.json: ", error)
+    console.error("Error fetching basket.json: ", error);
   }
 }
 
-function plusMinus() {
-  const allContainers = document.querySelectorAll('.container')
+function plusMinus(pcs) {
+  const allContainers = document.querySelectorAll(".container");
   allContainers.forEach((container) => {
-    const q = container.querySelector('.quantity')
-    console.log(q);
-    const buttons = q.querySelectorAll('button');
-    const label = q.querySelector('label');
-    console.log(buttons, label)
-    let i = 1;
-    for (let i = 0; i < buttons.length; i++) {
-      console.log(buttons[i])
-    }
-    // plus.addEventListener('click', () => {
-    //   label.innerText = i++;
-    //   i++;
-    // })
-    // minus.addEventListener('click', () => {
-    //   if (i > 0) {
-    //     label.innerText = i--;
-    //     i--;
-    //   } else {
-    //     label.innerText = 0;
-    //   }
-    // })
-  })
+    const q = container.querySelector(".quantity");
+    const buttons = q.querySelectorAll("button");
+    const label = q.querySelector("label");
+    let count = 1;
+    buttons.forEach((button) => {
+      if (button.innerText === "+") {
+        button.addEventListener("click", () => {
+          if (count < pcs) {
+            label.innerText = ++count;
+          }
+          if (count === pcs) {
+            button.disabled = true;
+          }
+          buttons[0].disabled = count <= 1;
+        });
+      } else if (button.innerText === "-") {
+        button.addEventListener("click", () => {
+          if (count > 1) {
+            label.innerText = --count;
+          }
+          buttons[1].disabled = count >= pcs;          
+          button.disabled = count <= 1;
+        });
+      }
+    });
+  });
+}
+
+function checkout() {
+  const root = document.querySelector('#root');
+  root.insertAdjacentHTML('beforeend', `<div class="checkout"><button id="check">Checkout</button></div>`)
+  const checkBtn = document.querySelector('#check');
 }
 
 function display(games, basket) {
-  const root = document.querySelector('#root');
+  const root = document.querySelector("#root");
   games.games.forEach((game) => {
     basket.basket.forEach((bId) => {
       let number = parseInt(bId.id);
       if (number === game.id) {
         let current = `${game.price}$`;
-      if (game.price === 0) {
-        current = "FREE";
-      }
-      root.insertAdjacentHTML(
-        "beforeend",
-        `
+        if (game.price === 0) {
+          current = "FREE";
+        }
+        root.insertAdjacentHTML(
+          "beforeend",
+          `
         <div class="container container${game.id}">
           <div><h2>${game.name}</h2>
             <h3>
@@ -75,25 +87,27 @@ function display(games, basket) {
             Stock: ${game.stock} pcs
           </p>  
           <div class="quantity">
-            <button class"minus">-</button>
+            <button class="minus">-</button>
             <label class="bq">1</label>
-            <button class"plus">+</button>
+            <button class="plus">+</button>
           </div>
         </div>
         `
-      );
+        );
+        plusMinus(game.stock);
       }
-    })
+    });
   });
-  plusMinus();
+  
 }
 
 async function loadEvent() {
   const games = await fetchGames();
   const basket = await fetchBasket();
-  console.log('games: ', games);
-  console.log('basket: ', basket)
-  display(games, basket)
+  console.log("games: ", games);
+  console.log("basket: ", basket);
+  display(games, basket);
+  checkout();
 }
 
 window.addEventListener("load", loadEvent);
